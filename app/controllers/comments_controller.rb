5ca06585f01560517c78b84df77cda1a3fb387d3
charlie <charlie@example.com>
2022-09-21 22:38:28 +0100

    done

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,16 +2,17 @@ class CommentsController < ApplicationController
     def create
         @comment = Comment.new(comment_params)
         @comment.user_id = session[:user_id]
-        @comment.recipe_id = 1
+        @comment.recipe_id
 
         if @comment.save
             flash[:success] = "Comment uploaded"
+            redirect_to "/recipes/#{@comment.recipe_id}"
         else
             render '/recipes/comment_upload_error'
         end
     end
     private
         def comment_params
-            params.require(:comment).permit(:content)
+            params.require(:comment).permit(:content, :recipe_id)
         end
 end
