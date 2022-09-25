2817c9ce7b4b8ed42b77db3c409d3d1192615cc0
charlie <charlie@example.com>
2022-09-25 23:01:12 +0100

    done

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,2 +1,17 @@
 module RecipesHelper
+     def average_rating(recipe)
+            array_of_recipe_ratings = []
+            for rating in Rating.where(recipe_id: recipe.id) do
+                array_of_recipe_ratings << rating.rating
+            end
+
+            total = 0.00
+        
+            for rating in array_of_recipe_ratings do
+                total += rating.to_f
+            end
+
+            average_rating = (total / (array_of_recipe_ratings.length)).round(2)
+            return average_rating
+        end
 end
